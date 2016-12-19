3e9b9024d9d90ab2feee458aebfe41ee085459b2
Victoria Frantsuh <eboko1@gmail.com>
2016-12-19 22:12:03 +0200

    Initial commit

---
+++ src/Main/Game.java
@@ -0,0 +1,14 @@
+package Main;
+
+import javax.swing.*;
+
+/**
+ * Created by Vika on 19.12.2016.
+ */
+public class Game {
+    public static void main(String[] args){
+        JFrame windows=new JFrame("Dragon Tale ");
+
+
+    }
+}
---
+++ src/Main/GamePanel.java
@@ -0,0 +1,7 @@
+package Main;
+
+/**
+ * Created by Vika on 19.12.2016.
+ */
+public class GamePanel {
+}
