32a4fea293e605cc374f103ddf174a5123d983b7
Victoria Frantsuh <eboko1@gmail.com>
2016-12-20 23:45:00 +0200

    add listener

--- src/Main/GamePanel.java
+++ src/Main/GamePanel.java
@@ -1,13 +1,17 @@
 package Main;
 
+import com.sun.corba.se.pept.transport.ListenerThread;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 
 /**
  * Created by Vika on 19.12.2016.
  */
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements  Runnable,KeyListener{
+
     //dimensions
     public  static final int WIDTH=320;
     public static final int HEIGHT=240;
