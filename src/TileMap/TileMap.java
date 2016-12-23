8d7e6bf66665cf3f50b131df4a3cd5f87ceaa8a4
Victoria Frantsuh <eboko1@gmail.com>
2016-12-23 23:06:08 +0200

    add TileMap

--- src/TileMap/TileMap.java
+++ src/TileMap/TileMap.java
@@ -1,5 +1,7 @@
 package TileMap;
 
+import java.awt.image.BufferedImage;
+
 /**
  * Created by Vika on 23.12.2016.
  */
@@ -15,6 +17,32 @@ public class TileMap {
     private int ymax;
 
     private double tween;
+
+    // map
+    private int [][]map;
+    private int tileSize;
+    private int numRows;
+    private int numCols;
+    private int width;
+    private int height;
+
+    // tile set
+    private BufferedImage tileSet;
+    private  int numTilesAcross;
+    private Tile[][] tiles;
+
+    // drawing
+    private int roeOffset;
+    private int colOffset;
+    private int numRowsToDraw;
+    private int numColsToDraw;
+
+    public TileMap(int tileSize){
+        
+
+    }
+
+
 
 
 }
