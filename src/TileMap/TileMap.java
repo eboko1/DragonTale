698ee64a2017a431dcf3062805a8bd1ffae30939
Victoria Frantsuh <eboko1@gmail.com>
2016-12-23 22:59:32 +0200

    add TileMap

---
+++ src/TileMap/TileMap.java
@@ -0,0 +1,20 @@
+package TileMap;
+
+/**
+ * Created by Vika on 23.12.2016.
+ */
+public class TileMap {
+    // position
+    private double x;
+    private double y;
+
+    //bounds
+    private int xmin;
+    private int ymin;
+    private int xmax;
+    private int ymax;
+
+    private double tween;
+
+
+}
