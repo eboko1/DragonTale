6ebee72a0394cbaffff28d238f4552947ec7c209
Victoria Frantsuh <eboko1@gmail.com>
2017-02-14 08:07:36 +0200

    add  Tile Map

--- src/TileMap/Background.java
+++ src/TileMap/Background.java
@@ -8,53 +8,58 @@ import javax.imageio.ImageIO;
 /**
  * Created by Vika on 22.12.2016.
  */
-public class Background {
+    public class Background {
 
-    private BufferedImage image;
-    private double x;
-    private double y;
-    private double dx;
-    private double dy;
+        private BufferedImage image;
+        private double x;
+        private double y;
+        private double dx;
+        private double dy;
 
-    private double moveScale;
+        private double moveScale;
 
-    public Background(String s, double ms){
+        public Background(String s, double ms){
 
-        try{
-       image= ImageIO.read(getClass().getResourceAsStream(s));
-            moveScale=ms;
-        }catch (Exception e){
-            e.printStackTrace();
+            try{
+                image= ImageIO.read(getClass().getResourceAsStream(s));
+                moveScale=ms;
+            }catch (Exception e){
+                e.printStackTrace();
+            }
         }
-    }
- public void setPosition(double x,double y){
-     this.x=(x*moveScale)%GamePanel.WIDTH;
-     this.y=(y*moveScale)%GamePanel.HEIGHT;
-      }
- public  void setVector (double dx, double dy){
-     this.dx=dx;
-     this.dy=dy;
- }
-public void update(){
-    x+=dx;  //x=x+dx;
-    y+=dy;  //y=y+dy;
-}
-public void draw(Graphics2D g){
-    // if(image==null){System.out.print("IMAGE NULL");
-    //System.exit(0);
-    //}
+        
+         public void setPosition(double x,double y){
+             this.x=(x*moveScale)%GamePanel.WIDTH;
+             this.y=(y*moveScale)%GamePanel.HEIGHT;
+         }
 
-    // if(g==null){System.out.print("g NULL");
-    //System.exit(0);
-    //}
+         public  void setVector (double dx, double dy){
+             this.dx=dx;
+             this.dy=dy;
+         }
 
-    g.drawImage(image,(int)x, (int) y, null);
-    if (x<0){
-        g.drawImage(image,(int)x+ GamePanel.WIDTH,(int) y, null);
-    }
-    if (x>0){
-        g.drawImage(image, (int)x-GamePanel.WIDTH,(int)y,null);
-    }
-}
+         public void update(){
+            x+=dx;  //x=x+dx;
+            y+=dy;  //y=y+dy;
+         }
 
-}
+         public void draw(Graphics2D g){
+            // if(image==null){System.out.print("IMAGE NULL");
+            //System.exit(0);
+            //}
+
+            // if(g==null){System.out.print("g NULL");
+            //System.exit(0);
+            //}
+
+            g.drawImage(image,(int)x, (int) y, null);
+            if (x<0){
+                g.drawImage(image,(int)x+ GamePanel.WIDTH,(int) y, null);
+            }
+            if (x>0){
+                g.drawImage(image, (int)x-GamePanel.WIDTH,(int)y,null);
+            }
+         }
+
+    }
+
