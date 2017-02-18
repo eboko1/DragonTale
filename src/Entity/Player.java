33b45a1e8583877dc102842b8e9a480301e07e05
Victoria Frantsuh <eboko1@gmail.com>
2017-02-18 23:32:46 +0200

    add  class Player

--- src/Entity/Player.java
+++ src/Entity/Player.java
@@ -2,6 +2,7 @@ package Entity;
 
 import TileMap.TileMap;
 
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
 /**
@@ -24,7 +25,27 @@ public class Player extends  MapObject {
     private int fireBallDamage;
     //private ArrayList<FireBall>fireBalls;
 
-    
+    //scratch
+    private boolean scratching;
+    private int scrachDamage;
+    private int scrachRange;
+
+
+    //gliding
+    private boolean gliding;
+
+    //animation
+    private ArrayList<BufferedImage[]> sprites;
+
+    // animations actions
+    private static final int IDLE = 0;
+    private static final int WALKING = 1;
+    private static final int JUMPING = 2;
+    private static final int FALLING = 3;
+    private static final int GLIDING = 4;
+    private static final int FIREBALL = 5;
+    private static final int SCRATCHING = 6;
+
 
     public Player(TileMap tm) {
         super(tm);
